7dbc7df7557706271e6d1f45ef6d2ab3c9ff0de0
gustavomirandag <gustavomirandag@gmail.com>
2019-08-08 22:36:16 -0300

    Simple Web Chat

---
+++ AmazingChatWebApp/Models/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmazingChatWebApp.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Profile Sender { get; set; }
+        public Profile Recipient { get; set; }
+        private string content; //Variável protegida
+        public string Content
+        {
+            get => content;
+            set
+            {
+                if (value != null)
+                    content = value;
+            }
+        }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ AmazingChatWebApp/Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmazingChatWebApp.Models
+{
+    public class Profile
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Color { get; set; }
+        public string PicUrl { get; set; }
+    }
+}
